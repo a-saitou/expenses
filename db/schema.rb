5ed88e7e20171f1687ede87efe5c283229158620
a-saitou <doredoko@gmail.com>
2016-08-12 12:55:15 +0000

    425

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160812070055) do
+
+  create_table "agreements", force: true do |t|
+    t.string   "name"
+    t.integer  "code"
+    t.integer  "orderer_id"
+    t.time     "constraction_date"
+    t.time     "complketion_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "calendars", force: true do |t|
+    t.date     "hiduke"
+    t.integer  "year"
+    t.integer  "month"
+    t.integer  "day"
+    t.boolean  "holiday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orderers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "staffs", force: true do |t|
+    t.string   "name"
+    t.string   "office"
+    t.integer  "order"
+    t.decimal  "wage"
+    t.date     "birthday"
+    t.date     "hire_date"
+    t.date     "leaving_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "works", force: true do |t|
+    t.integer  "calendar_id"
+    t.integer  "staff_id"
+    t.integer  "agreement_id"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.integer  "work_time"
+    t.integer  "over_time"
+    t.integer  "late_night_over_time"
+    t.integer  "paid_holiday"
+    t.integer  "agency_holiday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
